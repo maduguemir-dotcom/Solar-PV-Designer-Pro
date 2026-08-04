1738fcf5dd97329f580b0b0798f805370acbf6c2
Ibrahim Sani <maduguemir@gmail.com>
2026-08-04 21:59:10 +0300

    Update battery_model.py

--- app/battery_model.py
+++ app/battery_model.py
@@ -1,1 +1,15 @@
 
+def calculate_battery_capacity(
+    energy,
+    autonomy_days,
+    depth_of_discharge
+):
+    """
+    Calculate battery storage requirement
+    """
+
+    battery = (
+        energy * autonomy_days
+    ) / (depth_of_discharge / 100)
+
+    return battery
