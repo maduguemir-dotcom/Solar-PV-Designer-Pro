e3716bb0523696e7e29916223fc9010870db279e
Ibrahim Sani <maduguemir@gmail.com>
2026-08-04 22:03:14 +0300

    Update solar_calculator.py

--- app/solar_calculator.py
+++ app/solar_calculator.py
@@ -1,13 +1,8 @@
-
 def calculate_pv_size(
     energy,
     sun_hours,
     efficiency
 ):
-    """
-    Calculate required solar PV capacity
-    """
-
     efficiency_factor = efficiency / 100
 
     pv_size = energy / (
@@ -15,3 +10,19 @@ def calculate_pv_size(
     )
 
     return pv_size
+
+
+
+def calculate_number_of_panels(
+    pv_size,
+    panel_rating
+):
+    """
+    Calculate number of solar panels required
+    """
+
+    panel_kw = panel_rating / 1000
+
+    number = pv_size / panel_kw
+
+    return round(number)
